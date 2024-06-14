6315c41e705e69fde8ce91bd85037c2716c616c7
shivam <64252021+itsprogrammingwithme@users.noreply.github.com>
2024-06-14 10:47:11 +0530

    add

--- src/utils/utils.js
+++
@@ -1,8 +0,0 @@
-export const copyToClipboard = async (str) => {
-    try {
-      await navigator.clipboard.writeText(str);
-      alert('Copied to clipboard!');
-    } catch (error) {
-      console.error('Failed to copy:', error);
-    }
-  };
